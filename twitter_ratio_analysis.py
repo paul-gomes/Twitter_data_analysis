ae322101f90784701078a9a79d3094dd11e509d0
Anik Paul Gomes <gomesap29@gmail.com>
2019-03-29 06:56:09 -0500

    twitter ratio for messi works

--- twitter_ratio_analysis.py
+++ twitter_ratio_analysis.py
@@ -7,7 +7,8 @@ Created on Sun Feb 24 19:18:56 2019
 from twitter_keys_paul import consumer_key, consumer_secret, access_token, access_secret
 import matplotlib.pyplot as plt
 import tweepy
-import numpy as np
+import requests
+from bs4 import BeautifulSoup
 
 #Setting up Authentication
 
@@ -16,6 +17,31 @@ auth.set_access_token(access_token, access_secret)
 
 api = tweepy.API(auth)
 
+#Calculating twitter ration for twitter accounts -Messi, Ronaldo, Neymer
+
+#Twitter ratio for messi
+twitter_ratio_messi = []
+for status in tweepy.Cursor(api.user_timeline, id="imessi").items(1000):
+    print(status.text)
+    retweets = status.retweet_count
+    likes = status.favorite_count
+    status = 'https://twitter.com/imessi/status/{}'.format(status.id)
+    r = requests.get(status)
+    soup = BeautifulSoup(r.text, 'html5lib')
+    replies = soup.find('span', {"class": "ProfileTweet-actionCount"})
+    num_replies = int(replies['data-tweet-stat-count'])
+    print(retweets, likes, num_replies)
+    twitter_ratio_messi.append(round((num_replies/(likes + retweets)), 3))  
+    print(twitter_ratio_messi)
+    print("--------------------------------------------------------------------")
+        
+
+
+
+
+
+#Using tweepy module this way does not give correct number of replies
+"""
 #Calculating twitter ration for twitter accounts -Messi, Ronaldo, Neymer
 
 for status in tweepy.Cursor(api.user_timeline, id="imessi").items(1):
@@ -37,3 +63,4 @@ for status in tweepy.Cursor(api.user_timeline, id="imessi").items(1):
     print(retweets, likes, replies_count) 
         
     print("-------------------------------------")
+"""
